f4e4fa0d07b512e48d9a8a8ab67dcfe9354ac730
Roberto Molina <24000849@galileo.edu>
2026-09-18 08:06:23 -0600

    main corregido

--- G4_Test1/main.c
+++ G4_Test1/main.c
@@ -1,24 +1,38 @@
 #include "RTE_Components.h"
 #include CMSIS_device_header
-#include "USART.h"
+#include "LEDs.h"
 
+//frecuencia en hz a la que va parpadear el led
+#define FREQ   50
 
+//variable para contar cuantas vueltas ha dado el bucle while
+uint64_t ticks;
+
+uint8_t copy;
 
 int main(void){
-	
-	USART_config(115200);
-	
-	USART_putString("\n\r         Proyecto Final Microprocesadores 2022       \n\r");
-	USART_putString("                    Programa Monitor                 \n\r");
-	USART_putString("                      Julio Fajardo                  \n\r\n\r");
-	USART_putString(">> ");
-	
+	//habilita el reloj del GPIOB y configura el pin del LED como salida
+	LEDs_Init();
+
+	//SysTick es un temporizor de 24 bits
+	//configura el SysTick para que genere una interrupcion cada 1/FREQ segundos
+	SysTick_Config(SystemCoreClock/FREQ);
+
+	//para que el cpu no se quede sin hacer nada, se puede poner un bucle infinito que cuente ticks
 	while(1){
-		if(cmd_ready){
-			USART_putString("\n\rUn comando ha llegado!!!\n\r");
-			cmd_ready = 0;
-		}
+		ticks++;
 	}
 }
 
+//rutina de servcio de la interrupcion del SysTick, se ejecuta cada 1/FREQ segundos
+void SysTick_Handler(void){
+	// la variable sirve para contar cuantas veces se ha ejecutado la interrupcion
+	// static conserva su valor entre llamadas a la funcion
+	static uint32_t data;
+	data++;
+	copy = data;
+	//invierte el estado del led cada vez que se ejecuta la interrupcion
+	//usando XOR con el registro ODR del GPIOB
+	LED_Toggle();
+}
 
